9f559cbb9867702890a0b3ad991b4e7adaefa50b
Nishant Joshi <nishant@pop-os.localdomain>
2023-01-26 23:18:35 +0530

    selenium for top 50 songs

--- echelon.py
+++ echelon.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
-import requests
-from collections import namedtuple
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import time
+import json
 
+# Number of songs from top charts
+SONG_LIMIT = 50
 
 def extractText(arr, mode):
+    '''arr: [rank, title, artist, views, link] for each song'''
     if mode == 'listofdicts':
         songs = []
         for rank, title, artist, views, link in arr:
@@ -20,26 +26,37 @@ def extractText(arr, mode):
         
         return songs
 
+# Don't open firefox window on screen
+options = FirefoxOptions()
+options.add_argument('--headless')
 
-r = requests.get('https://genius.com')
+driver = webdriver.Firefox(options=options)
+driver.get('https://genius.com')
+
+# 
+for _ in range(SONG_LIMIT // 10 - 1):
+    elem = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[5]/div[2]/div/div[3]/div')
+    elem.click()
+    time.sleep(5) # give 5 seconds rest between first click and finding the [see more] button again
 
 
-soup = BeautifulSoup(r.text, 'html.parser')
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-
+# findAll -> param1 : tag , param2 : class of the tag
 artistnames = soup.findAll('h4', 'ChartSongdesktop__Artist-sc-18658hh-5 kiggdb')
 songnames = soup.findAll('div', 'ChartSongdesktop__Title-sc-18658hh-3 fODYHn')
 views = soup.findAll('span', 'TextLabel-sc-8kw9oj-0 knRXtG')
 ranks = soup.findAll('div', 'ChartItemdesktop__Rank-sc-3bmioe-1 tDViA')
 links = soup.findAll('a', 'PageGriddesktop-a6v82w-0 ChartItemdesktop__Row-sc-3bmioe-0 qsIlk')
 
-chart = []
 
+chart = []
 for i in range(len(artistnames)):
     song = [ranks[i],songnames[i],artistnames[i],views[i],links[i]]
     chart.append(song)
 
-
 songs = extractText(chart, 'listofdicts')
-for song in songs:
-    print(song)
+
+# write charts in charts.txt file in json format
+with open('charts.txt', 'w') as f:
+    f.write(json.dumps(songs))
